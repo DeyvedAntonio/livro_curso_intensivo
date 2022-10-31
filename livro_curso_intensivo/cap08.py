6a8c70fe7302815d76b33809cc8cce22ec2909d3
Deyved Antonio <deyved.antonio@gmail.com>
2022-10-31 11:27:30 -0300

    Exercício 8.3

--- livro_curso_intensivo/cap08.py
+++ livro_curso_intensivo/cap08.py
@@ -9,6 +9,12 @@ make_shirt('G', 'Atividades de ramificação')
 make_shirt(texto='Não tenha pressa, mas não perca tempo', tamanho='GG')
 
 # 8.2
+def make_shirt(texto='Eu amo Python', tamanho='G'):
+    print(f'A frase escolhida foi: {texto} e o tamanho é {tamanho}.')
 
+
+make_shirt()
+make_shirt(tamanho='M')
+make_shirt('I love Python', 'P')
 
 # 8.3

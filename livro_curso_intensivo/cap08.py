a9fa924aa12a4be527aae3b21fe56aa2aa415856
Deyved Antonio <deyved.antonio@gmail.com>
2022-11-14 13:40:05 -0300

    resolução do exercício 8.9

--- livro_curso_intensivo/cap08.py
+++ livro_curso_intensivo/cap08.py
@@ -1,61 +1,69 @@
 # Exercícios do capítulo 08
 
 # 8.3
-def make_shirt(tamanho, texto):
-    print(f'A frase escolhida foi: {texto} e o tamanho é {tamanho}.')
+# def make_shirt(tamanho, texto):
+#     print(f'A frase escolhida foi: {texto} e o tamanho é {tamanho}.')
 
 
-make_shirt('G', 'Atividades de ramificação')
-make_shirt(texto='Não tenha pressa, mas não perca tempo', tamanho='GG')
+# make_shirt('G', 'Atividades de ramificação')
+# make_shirt(texto='Não tenha pressa, mas não perca tempo', tamanho='GG')
 
-# 8.4
-def make_shirt(texto='Eu amo Python', tamanho='G'):
-    print(f'A frase escolhida foi: {texto} e o tamanho é {tamanho}.')
-
-
-make_shirt()
-make_shirt(tamanho='M')
-make_shirt('I love Python', 'P')
-
-# 8.5
-def descibe_city(cidade, pais='Brasil'):
-    print(f'{cidade} está localizada no {pais}')
+# # 8.4
+# def make_shirt(texto='Eu amo Python', tamanho='G'):
+#     print(f'A frase escolhida foi: {texto} e o tamanho é {tamanho}.')
 
 
-descibe_city('Cariaica')
-descibe_city('Lisboa', 'Portugal')
-descibe_city('Recife')
+# make_shirt()
+# make_shirt(tamanho='M')
+# make_shirt('I love Python', 'P')
 
-# 8.6
-def city_country(city, country):
-    return f'{city.title()}, {country.title()}'
-
-
-print(city_country('santiago', 'chile'))
-print(city_country('recife', 'brasil'))
-print(city_country('lisboa', 'portugal'))
-
-# 8.7
-def make_album(artist, title_album):
-    info = {'Artist': artist.title(), 'Title': title_album.title()}
-    return info
+# # 8.5
+# def descibe_city(cidade, pais='Brasil'):
+#     print(f'{cidade} está localizada no {pais}')
 
 
-album_0 = make_album('pitty', 'amanhã')
-album_1 = make_album('disturbed', 'atention')
-album_2 = make_album('system of down', 'system')
+# descibe_city('Cariaica')
+# descibe_city('Lisboa', 'Portugal')
+# descibe_city('Recife')
 
-print(album_0, '\n', album_1, '\n', album_2)
+# # 8.6
+# def city_country(city, country):
+#     return f'{city.title()}, {country.title()}'
 
-# 8.8
-artistas = {}
-while True:
-    artista = input('Informe o nome do artista (caso queira sair digite "q" a qualquer momento): ')
-    if artista == 'q':
-        break
-    album = input('Informe o título do álbum: ')
-    if album == 'q':
-        break
-    artistas = make_album(artista, album)
 
-print(f'O dicionário criado é {artistas}')
+# print(city_country('santiago', 'chile'))
+# print(city_country('recife', 'brasil'))
+# print(city_country('lisboa', 'portugal'))
+
+# # 8.7
+# def make_album(artist, title_album):
+#     info = {'Artist': artist.title(), 'Title': title_album.title()}
+#     return info
+
+# album_0 = make_album('pitty', 'amanhã')
+# album_1 = make_album('disturbed', 'atention')
+# album_2 = make_album('system of down', 'system')
+
+# print(album_0, '\n', album_1, '\n', album_2)
+
+# # 8.8
+# artistas = {}
+# while True:
+#     artista = input('Informe o nome do artista (caso queira sair digite "q" a qualquer momento): ')
+#     if artista == 'q':
+#         break
+#     album = input('Informe o título do álbum: ')
+#     if album == 'q':
+#         break
+#     artistas = make_album(artista, album)
+
+# print(f'O dicionário criado é {artistas}')
+
+# 8.9
+magicians = ['David', 'Fred', 'Sarah']
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
+show_magicians(magicians)

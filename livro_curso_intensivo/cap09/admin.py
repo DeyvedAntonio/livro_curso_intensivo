8e1466f534f4813f98a9b17f3d7ad77a27792421
Deyved Antonio <deyved.antonio@gmail.com>
2023-01-07 15:07:08 -0300

    resolução do exercício 9.8

--- livro_curso_intensivo/cap09/admin.py
+++ livro_curso_intensivo/cap09/admin.py
@@ -1,4 +1,5 @@
 from user import User
+from privileges import Privileges
 
 class Admin(User):
     """Classe criada para resolver o exercício 9.7."""
@@ -9,10 +10,15 @@ class Admin(User):
         self.privileges = privileges
 
 
-    def show_privileges(self):
-        """Lista o conjunto de privilégios de um administrador."""
-        print(self.username, self.privileges)
+    # def show_privileges(self):
+    #     """Lista o conjunto de privilégios de um administrador."""
+    #     print(self.username, self.privileges)
 
 
 admin_0 = Admin('Deyved', 'Silva', 'deyved@silva', 'D_S', 'can delete post', 'can ban user')
-admin_0.show_privileges()
+# admin_0.show_privileges()
+admin_1 = Admin('Davi', 'Silva', 'davi@silva', 'D-S', 'can add post')
+admin_1.privileges = Privileges('can delete post', 'can ban user')
+admin_0.describe_user()
+admin_1.describe_user()
+admin_1.privileges.show_privileges()

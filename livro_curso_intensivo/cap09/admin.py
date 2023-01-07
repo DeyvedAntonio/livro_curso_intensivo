492a21ac3fa92d562e1d1475c29a0a5776b371b4
Deyved Antonio <deyved.antonio@gmail.com>
2023-01-07 14:47:41 -0300

    resolução do exercício 9.7

--- livro_curso_intensivo/cap09/admin.py
+++ livro_curso_intensivo/cap09/admin.py
@@ -2,8 +2,17 @@ from user import User
 
 class Admin(User):
     """Classe criada para resolver o exercício 9.7."""
+    
     def __init__(self, first_name, last_name, email, username, *privileges):
+        """Inicializa os atributos da classe."""
         super().__init__(first_name, last_name, email, username)
         self.privileges = privileges
-    
 
+
+    def show_privileges(self):
+        """Lista o conjunto de privilégios de um administrador."""
+        print(self.username, self.privileges)
+
+
+admin_0 = Admin('Deyved', 'Silva', 'deyved@silva', 'D_S', 'can delete post', 'can ban user')
+admin_0.show_privileges()
